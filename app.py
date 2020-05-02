ed05337a0c70a3863ac9065650a6ab943cf0a3c2
denis <denis66604@gmail.com>
2020-05-02 17:41:15 +0800

    Add code

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ config.read('config.ini')
 
 line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
 handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
+URL = config.get('line-bot','URL')
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -44,7 +45,7 @@ def handle_message(event):
         #Your user ID
 
     elif(text=="直播連結"):
-        reply_text = 'URL'
+        reply_text = URL
     elif(text=="機器人"):
         reply_text = "叫我嗎"
     else:
