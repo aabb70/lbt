1665b182bd6f3c81f124ef184143e13a3c7b42ee
denis <denis66604@gmail.com>
2020-05-02 17:16:11 +0800

    Add code

--- app.py
+++ app.py
@@ -7,13 +7,16 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import *
+import configparser
 
 app = Flask(__name__)
 
-# Channel Access Token
-line_bot_api = LineBotApi('GRY/AVVSBzJ+nc9Bc6LyRNTqH1uRDv19/5BsVoXZTHhGA7jJqJGMKS4zirw8SAEA/Wq6DIZ1JzIN/OsunHN3BK9y7tTcL1kDTYllnhaDpvqb3qfqYKZt2CkdpMCedewwap7jivnXdnC8arnKp+tWZAdB04t89/1O/w1cDnyilFU=')
-# Channel Secret
-handler = WebhookHandler('83135cb6cbcc9edb9dbae71a59566788')
+# 讀取config
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
+handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -33,7 +36,22 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
+    print(event)
+    text=event.message.text
+
+    if (text=="Hi"):
+        reply_text = "Hello"
+        #Your user ID
+
+    elif(text=="你好"):
+        reply_text = "哈囉"
+    elif(text=="機器人"):
+        reply_text = "叫我嗎"
+    else:
+        reply_text = text
+#如果非以上的選項，就會學你說話
+
+    message = TextSendMessage(reply_text)
     line_bot_api.reply_message(event.reply_token, message)
 
 import os
