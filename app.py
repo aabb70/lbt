cbab1127bef0a5f831da712c17b493b55aa37957
denis <denis66604@gmail.com>
2020-05-02 19:22:20 +0800

    Add code

--- app.py
+++ app.py
@@ -29,6 +29,9 @@ def callback():
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
     # handle webhook body
+
+    print(body)
+    
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
