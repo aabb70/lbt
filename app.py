594e8455f1147d9865a8e601f07eda734ab33677
denis <denis66604@gmail.com>
2020-05-03 18:48:12 +0800

    Add code

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def handle_message(event):
             ]
         )
     )
-    line_bot_api.reply_message(event.reply_token, buttons_template)
+    line_bot_api.reply_message(event.reply_token, buttons_template):
     else:
         reply_text = text
 #如果非以上的選項，就會學你說話
