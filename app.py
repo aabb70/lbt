8a2281b35fb391134a2383cf84d753bed548d384
denis <denis66604@gmail.com>
2020-05-03 20:08:14 +0800

    Add code

--- app.py
+++ app.py
@@ -44,26 +44,23 @@ def callback():
 def handle_message(event):
     print(event)
     text=event.message.text
-    if (text=="Hi"):
+    if (text=="查詢商品"):
         message = TemplateSendMessage(
         alt_text='Buttons template',
         template=ButtonsTemplate(
-        thumbnail_image_url='https://example.com/image.jpg',
-        title='Menu',
-        text='Please select',
+        thumbnail_image_url='https://2.bp.blogspot.com/-juSspSqB2mo/UF1jQJlaHKI/AAAAAAAAAOo/62CPquAChjA/s1600/9477o2.jpg',
+        title='查詢商品',
+        text='請選擇查詢的類別',
         actions=[
-            PostbackTemplateAction(
-                label='postback',
-                text='postback text',
-                data='action=buy&itemid=1'
-            ),
-            MessageTemplateAction(
-                label='message',
-                text='message text'
-            ),
             URITemplateAction(
-                label='uri',
-                uri='http://example.com/'
+                label='吉娃娃',
+                uri='https://2.bp.blogspot.com/-juSspSqB2mo/UF1jQJlaHKI/AAAAAAAAAOo/62CPquAChjA/s1600/9477o2.jpg'
+            ),URITemplateAction(
+                label='珍珠奶茶',
+                uri='https://2.bp.blogspot.com/-juSspSqB2mo/UF1jQJlaHKI/AAAAAAAAAOo/62CPquAChjA/s1600/9477o2.jpg'
+            ),URITemplateAction(
+                label='外星狗',
+                uri='https://2.bp.blogspot.com/-juSspSqB2mo/UF1jQJlaHKI/AAAAAAAAAOo/62CPquAChjA/s1600/9477o2.jpg'
             )
         ]
     )
