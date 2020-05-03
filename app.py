7f1968a1e1cbc58b397a768f443c7e25d2d8bff4
denis <denis66604@gmail.com>
2020-05-03 19:10:06 +0800

    Add code

--- app.py
+++ app.py
@@ -56,31 +56,30 @@ def handle_message(event):
         reply_text = "https://shopee.tw/search?keyword=%E6%89%8B%E6%A9%9F&shop=14084056"
     elif(text=="零食"):
         reply_text = "https://shopee.tw/shop/14084056/search?shopCollection=3801770"
-    elif (text == "查詢商品"):
-        button_template_message =ButtonsTemplate(
-                            thumbnail_image_url="https://i.imgur.com/eTldj2E.png?1",
-                            title='Menu', 
-                            text='Please select',
-                            ratio="1.51:1",
-                            image_size="cover",
-                            actions=[
-#                                PostbackTemplateAction 點擊選項後，
-#                                 除了文字會顯示在聊天室中，
-#                                 還回傳data中的資料，可
-#                                 此類透過 Postback event 處理。
-                                PostbackTemplateAction(
-                                    label='postback還會回傳data參數', 
-                                    text='postback text',
-                                    data='action=buy&itemid=1'
-                                ),
-                                MessageTemplateAction(
-                                    label='message會回傳text文字', text='message text'
-                                ),
-                                URITemplateAction(
-                                    label='uri可回傳網址', uri='http://www.xiaosean.website/'
-                                )
-                            ]
-                        )
+     elif event.message.text == "Buttons Template":
+        buttons_template = TemplateSendMessage(
+        alt_text='Buttons Template',
+        template=ButtonsTemplate(
+            title='這是ButtonsTemplate',
+            text='ButtonsTemplate可以傳送text,uri',
+            thumbnail_image_url='顯示在開頭的大圖片網址',
+            actions=[
+                MessageTemplateAction(
+                    label='ButtonsTemplate',
+                    text='ButtonsTemplate'
+                ),
+                URITemplateAction(
+                    label='VIDEO1',
+                    uri='影片網址'
+                ),
+                PostbackTemplateAction(
+                    label='postback',
+                    text='postback text',
+                    data='postback1'
+                )
+            ]
+        )
+    )
     else:
         reply_text = text
 #如果非以上的選項，就會學你說話
