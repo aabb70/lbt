0ff29c81fd8982e7ed93d3fe29490f9323f62a54
denis <denis66604@gmail.com>
2020-05-07 23:21:54 +0800

    Add code

--- app.py
+++ app.py
@@ -116,10 +116,10 @@ if __name__ == "__main__":
     app.run(host='0.0.0.0', port=port)
 
 def sendBack_sell(event, backdata):  #處理Postback
-try:
-    message = TextSendMessage(  #傳送文字
-        text = '點選的是賣 ' + backdata.get('item')
-    )
-    line_bot_api.reply_message(event.reply_token, message)
-except:
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+    try:
+        message = TextSendMessage(  #傳送文字
+            text = '點選的是賣 ' + backdata.get('item')
+        )
+        line_bot_api.reply_message(event.reply_token, message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
