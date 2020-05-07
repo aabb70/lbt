3dbe64ac54a81f9cec93b8bc0898349a9cbbc401
denis <denis66604@gmail.com>
2020-05-08 00:17:59 +0800

    Add code

--- views.py
+++ views.py
@@ -2,44 +2,14 @@ from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import MessageEvent, TextMessage, PostbackEvent
 from urllib.parse import parse_qsl
+import configparser
 
 line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
 parser = WebhookHandler(config.get('line-bot', 'channel_secret'))
 
 @csrf_exempt
 def callback(request):
-    if request.method == 'POST':
-        signature = request.META['HTTP_X_LINE_SIGNATURE']
-        body = request.body.decode('utf-8')
-        try:
-            events = parser.parse(body, signature)
-        except InvalidSignatureError:
-            return HttpResponseForbidden()
-        except LineBotApiError:
-            return HttpResponseBadRequest()
-
         for event in events:
-            if isinstance(event, MessageEvent):
-                if isinstance(event.message, TextMessage):
-                    mtext = event.message.text
-                    if mtext == '@按鈕樣板':
-                        func.sendButton(event)
-    
-                    elif mtext == '@確認樣板':
-                        func.sendConfirm(event)
-    
-                    elif mtext == '@轉盤樣板':
-                        func.sendCarousel(event)
-    
-                    elif mtext == '@圖片轉盤':
-                        func.sendImgCarousel(event)
-    
-                    elif mtext == '@購買披薩':
-                        func.sendPizza(event)
-    
-                    elif mtext == '@yes':
-                        func.sendYes(event)
-    
             if isinstance(event, PostbackEvent):  #PostbackTemplateAction觸發此事件
                 backdata = dict(parse_qsl(event.postback.data))  #取得Postback資料
                 if backdata.get('action') == 'buy':
@@ -51,6 +21,7 @@ def callback(request):
 
     else:
         return HttpResponseBadRequest()
+
 def sendBack_buy(event, backdata):  #處理Postback
     try:
         text1 = '感謝您購買披薩，我們將盡快為您製作。\n(action 的值為 ' + backdata.get('action') + ')'
