c9cc39122bb2c9ea1debe4e5ebfe9e2cd6a4fddc
denis <denis66604@gmail.com>
2020-05-08 15:35:44 +0800

    Add code

--- app.py
+++ app.py
@@ -143,7 +143,7 @@ def handle_message(event):
     
     line_bot_api.reply_message(event.reply_token, message)
 def sendBack_QA1(event, backdata):  #處理Postback
-   try:
+    try:
         message = TextSendMessage(  #傳送文字
             text = "營業時間週一~週五 14:00-22:00 週六 15:00-20:00 週日與例假日不定時公休,如需面交自取可先來電(02)2388-8488洽詢。" + backdata.get('QS1')
         )
