637564f394b8394f63599fa4e150c6b72a08ab97
denis <denis66604@gmail.com>
2020-05-08 14:14:46 +0800

    Add code

--- app.py
+++ app.py
@@ -80,6 +80,34 @@ def handle_message(event):
             original_content_url = "https://cdn.discordapp.com/attachments/682086463265177652/707961786938425434/70756594_2871486492880328_5131557763402432512_n.jpg",
             preview_image_url = "https://fairmedia.tw/wp-content/uploads/20191112002764.jpg"
         )
+    elif(text=="@常見問題"):
+        message = TextSendMessage(
+        text = '你晚餐想吃什麼？',
+        quick_reply = QuickReply(
+            items = [
+                QuickReplyButton(
+                    action = MessageAction(label = "泡麵", text = "自己煮！！"),
+                    image_url = 'http://shareboxnow.com/wp-content/uploads/2020/01/S__7938233.jpg'
+                ),
+                QuickReplyButton(
+                    action = MessageAction(label = "火鍋", text = "自己買！！"),
+                ),
+                QuickReplyButton(
+                    action = MessageAction(label = "牛排", text = "自己煎！！"),
+                ),
+                QuickReplyButton(
+                    action = MessageAction(label = "炒麵", text = "炒起來！！"),
+                ),
+                QuickReplyButton(
+                    action = MessageAction(label = "鐵板燒", text = "我不會！！"),
+                ),
+                QuickReplyButton(
+                    action = MessageAction(label = "生魚片", text = "該去漁港了！！"),
+                )
+            ]
+        )
+    )
+        
     elif(text=="@促銷商品"):
         message = TemplateSendMessage(
             alt_text='圖片轉盤樣板',
@@ -112,7 +140,7 @@ def handle_message(event):
         reply_text = "https://reurl.cc/Qd56r0\n↑使用蝦皮聊聊來聯絡我們\n\nhttps://reurl.cc/4R63KV\n↑使用Facebook粉絲專業聯絡我們\n\nhttps://reurl.cc/yZDe2l\n↑使用Instagram來聯絡我們"
         message = TextSendMessage(reply_text)
     else:
-        reply_text = text
+        reply_text = "如找不到您所想找的東西，請輸入 @幫助 他會直接回覆您。"
         message = TextSendMessage(reply_text)
     
     line_bot_api.reply_message(event.reply_token, message)
