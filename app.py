efdd83af1971a062ba3f5c76bbc26adeaccfcbf7
denis <denis66604@gmail.com>
2020-05-22 01:53:59 +0800

    Add code

--- app.py
+++ app.py
@@ -136,7 +136,7 @@ def handle_message(event):
                     ),
                 ]
         )
-        line_bot_api.reply_message(event.reply_token,message)
+    line_bot_api.reply_message(event.reply_token,message)
     elif(text=="@促銷商品"):
         message = TemplateSendMessage(
             alt_text='圖片轉盤樣板',
