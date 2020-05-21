525ad8349471d6ec048e3bcdb71a07986ae441f7
denis <denis66604@gmail.com>
2020-05-22 02:12:40 +0800

    Add code

--- app.py
+++ app.py
@@ -50,9 +50,9 @@ def sendImgmap(event):  #圖片地圖
                 URIImagemapAction(  #開啟網頁
                     link_uri='https://reurl.cc/exp63b',
                     area=ImagemapArea(  #右方1/4區域(藍色1)
-                        x=imgwidth, 
+                        x=0, 
                         y=0, 
-                        width=imgwidth, 
+                        width=0, 
                         height=imgheight  
                     )
                 ),
